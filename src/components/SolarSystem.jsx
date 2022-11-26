90aef368a62a95f1eb76be054c4eed24626bd334
Marlon Couto <marloncouto199@gmail.com>
2022-11-25 22:32:06 -0300

    Renderiza uma lista com planetas do Sistema Solar
    Signed-off-by: Marlon Couto <marloncouto199@gmail.com>

--- src/components/SolarSystem.jsx
+++ src/components/SolarSystem.jsx
@@ -1,11 +1,23 @@
 import React from 'react';
 import Title from './Title';
+import planets from '../data/planets';
+import PlanetCard from './PlanetCard';
 
 export default class SolarSystem extends React.Component {
   render() {
     return (
       <div data-testid="solar-system">
         <Title headline="Planetas" />
+        {planets.map((planet) => {
+          const { name, image } = planet;
+          return (
+            <PlanetCard
+              key={ name }
+              planetName={ name }
+              planetImage={ image }
+            />
+          );
+        })}
       </div>
     );
   }
